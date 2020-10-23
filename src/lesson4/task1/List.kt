f28318fa0f03d57ba4aa41e083ebd8956bb9245b
Yura <chablin.yua@edu.spbstu.ru>
2020-10-23 15:26:48 +0300

    lesson 3 fib, revert, squareSequenceDigit, fibSequenceDigit done, lesson 4 roman done

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -241,7 +241,22 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String = TODO()
+fun roman(n: Int): String {
+    var result = ""
+    var numberDuplicate = n
+    val ListArabic = listOf(1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000)
+    val ListRoman = listOf("I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M")
+    while (numberDuplicate > 0) {
+        for (i in ListRoman.size - 1 downTo 0) {
+            if (numberDuplicate - ListArabic[i] >= 0) {
+                result += ListRoman[i]
+                numberDuplicate -= ListArabic[i]
+                break
+            }
+        }
+    }
+    return result
+}
 
 /**
  * Очень сложная (7 баллов)
